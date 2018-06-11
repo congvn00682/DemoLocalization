e2492c218f8302243ea059ebe07b36c050cd203c
Vu Ngoc Cong <congvn00682@gmail.com>
2018-06-11 09:39:21 +0700

    localization

---
+++ DemoLocalization/DataServices.swift
@@ -0,0 +1,15 @@
+//
+//  DataServices.swift
+//  DemoLocalization
+//
+//  Created by Vu Ngoc Cong on 5/29/18.
+//  Copyright © 2018 Vu Ngoc Cong. All rights reserved.
+//
+
+import UIKit
+
+class DataServices {
+    static let shared: DataServices = DataServices()
+    
+    
+}
---
+++ DemoLocalization/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  DemoLocalization
+//
+//  Created by Vu Ngoc Cong on 5/29/18.
+//  Copyright © 2018 Vu Ngoc Cong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+//    let alertTitle = NSLocalizedString("Welcome", comment: "")
+//    let alertMessage = NSLocalizedString("Thank you for trying this app, you are a great person!", comment: "")
+//    let cancelButtonText = NSLocalizedString("Cancel", comment: "")
+//    let signupButtonText = NSLocalizedString("Login", comment: "")
+    
+    //    func alert() {
+    //        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: UIAlertControllerStyle.alert)
+    //        let cancelAction = UIAlertAction(title: cancelButtonText, style: UIAlertActionStyle.cancel, handler: nil)
+    //        let signupAction = UIAlertAction(title: signupButtonText, style: UIAlertActionStyle.default, handler: nil)
+    //        alert.addAction(cancelAction)
+    //        alert.addAction(signupAction)
+    //        present(alert, animated: true, completion: nil)
+    //    }
+    
+    
+    @IBOutlet weak var addNew: UIButton!
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+
+}
+
